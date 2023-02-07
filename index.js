ea77abb88185e6dc93b72546db1dfd7e0f15ba61
Vladins3ki <123756024+Vladins3ki@users.noreply.github.com>
2023-02-07 22:22:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 
 let word, cont, wrong = -1;
 function getWord(){
-word = document.getElementById("txt").value;
-cont = word.length;
-for(let i = 0;i < word.length; ++i){
-        let div = document.createElement("div");
-        div.className="lettersField";
-        div.id= i;
-        document.getElementById("cont").appendChild(div);
-    }
-for(let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); ++i) {
-        let button = document.createElement('button')
-        button.className = "button"
-        button.id = "btn"
-        button.innerHTML = String.fromCharCode(i);
-        button.value = String.fromCharCode(i + 32);
-        button.addEventListener('click', () => {
-                compare(button.value);    
-                button.disabled = true;
-        })
-        document.getElementById("letters").appendChild(button);
-    }
-for(let i = 0; i < word.length; ++i){
-            if(word[0] == word[i]){
-            document.getElementById(i).innerHTML = word[i];
-            --cont;
+        word = document.getElementById("txt").value;
+        cont = word.length;
+        for(let i = 0;i < word.length; ++i){
+                let div = document.createElement("div");
+                div.className="lettersField";
+                div.id= i;
+                document.getElementById("cont").appendChild(div);
         }
-    }
-    document.getElementById("txt").value = "";
+        for(let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); ++i) {
+                let button = document.createElement('button')
+                button.className = "button"
+                button.id = "btn"
+                button.innerHTML = String.fromCharCode(i);
+                button.value = String.fromCharCode(i + 32);
+                button.addEventListener('click', () => {
+                        compare(button.value);    
+                        button.disabled = true;
+                })
+                document.getElementById("letters").appendChild(button);
+        }
+        for(let i = 0; i < word.length; ++i){
+                if(word[0] == word[i]){
+                        document.getElementById(i).innerHTML = word[i];
+                        --cont;
+                }
+        }
+       document.getElementById("txt").value = "";
 }
 function compare(value){
     for(let i = 0; i < word.length; ++i){
